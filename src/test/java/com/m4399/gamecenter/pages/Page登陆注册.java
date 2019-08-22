a448f8be326a3dd6112dd1effd1c4d28d2169974
Zhikai Ye <zhikaiye939@gmail.com>
2019-08-22 18:58:58 +0800

    更新代码

--- src/test/java/com/m4399/gamecenter/pages/Page登陆注册.java
+++ src/test/java/com/m4399/gamecenter/pages/Page登陆注册.java
@@ -1,20 +1,29 @@
 package com.m4399.gamecenter.pages;
 
+import com.m4399.gamecenter.driver.Driver;
 import org.openqa.selenium.By;
 
-public class Page登陆注册 extends BasePage{
+public class Page登陆注册 extends BasePage {
     By et_username = byXpathAndId("et_username");
     By et_password = byXpathAndId("et_password");
     By login_button = byXpathAndId("login_button");
 
-    public Page我页 成功登陆(String username,String password){
-
+    /**
+     * 1、登陆成功返回toast："登陆成功"；
+     * 2、登陆失败则根据具体toast提示进行返回；
+     *
+     * @return
+     */
+    public String 登陆(String username, String password) {
         find(et_username).sendKeys(username);
         find(et_password).sendKeys(password);
         find(login_button).click();
-
-        return new Page我页();
+        String toast = getToast();
+        try {
+            elementWait(Driver.getCurrentDriver(), 10, By.id("com.m4399.gamecenter:id/tv_user_name")).isDisplayed();
+            return "登陆成功";
+        } catch (Exception e) {
+            return toast;
+        }
     }
-
-
 }
--- src/test/java/com/m4399/gamecenter/testcase/Case01登陆.java
+++ src/test/java/com/m4399/gamecenter/testcase/Case01登陆.java
@@ -1,5 +1,6 @@
 package com.m4399.gamecenter.testcase;
 
+import com.m4399.gamecenter.driver.Driver;
 import com.m4399.gamecenter.pages.Page找游戏;
 import org.testng.annotations.*;
 
@@ -12,6 +13,11 @@ public class Case01登陆 {
         System.out.println("This is beforetest");
     }
 
+    @AfterTest
+    public void tearDown(){
+        Driver.getCurrentDriver().quit();
+    }
+
     @BeforeClass
     public void beforeclass(){
         System.out.println("This is beforeclass");
@@ -21,13 +27,19 @@ public class Case01登陆 {
     public void beforemethod(){
         System.out.println("this is beforemethod");
     }
+    @DataProvider(name = "haha")
+    public Object[][] logindata(){
+        Object[][] objects = new Object[][]{
+                {"2017011111","124567"},
+                {"2017011117","123456"}
+        };
+        return objects;
+    }
 
-    @Test
-    public void 登陆() throws MalformedURLException, InterruptedException {
-
+    @Test(dataProvider = "haha")
+    public void 登陆(String username,String password) throws MalformedURLException {
         Page找游戏 page找游戏 = new Page找游戏().进入找游戏首页();
-        page找游戏.点击我().点击登陆().成功登陆("2017011159","123456");
-
+        System.out.println(page找游戏.点击我().点击登陆().登陆(username, password));
     }
 
     @Test
